eed1361c08c03b6f50e2c6fa442df32cbab9e56c
Theodore <theodore.niu@gmail.com>
2025-04-11 09:32:49 +0800

    fix avatar bg

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -99,7 +99,7 @@ const Avatar: React.FC = () => {
       console.log('avatarConfig: ' + JSON.stringify(avatarConfig));
       avatarConfig.customized = false;
       avatarConfig.backgroundImage = new URL(
-        'https://playground.azuretsp.com/images/bg_dark.jpg',
+        'https://insider.playground.azuretsp.com/images/avatar_bg.jpg',
       );
 
       let responseData = null;
